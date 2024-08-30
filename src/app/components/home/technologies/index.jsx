9443aa152b1226ffd2ad6274ef1e327c174f0d9e
Abdvlljsx <alimiabdullahi93@gmail.com>
2024-08-30 23:30:55 +0100

    fixed distorted backgrounds

--- src/app/components/home/technologies/index.jsx
+++ src/app/components/home/technologies/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const Index = () => {
     return (
         <section className="mt-[3rem] relative">
-            <img src="/BrainB.webp" className="filter absolute top-[17rem] z-[-1] left-0 w-[20rem] hidden sm:block" alt="" />
+            <img src="/BrainB.webp" className="opacity-[.5] mix-blend-lighten absolute top-[17rem] z-[-1] left-0 w-[20rem] hidden sm:block" alt="" />
             <img src="/faceL.png" className="filter absolute  blend top-[0rem] hidden sm:block z-[-1] right-0 w-[20rem]" alt="" />
             <div className="px-[3rem] sm:px-[10rem]">
                 <div className="flex">
