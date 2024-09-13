392ea578e24ebc0b37eefc0ec4aac5313fa7da94
Clint <thunderdragonshoujo@gmail.com>
2024-09-12 20:37:33 -0500

    added metadata for seo

--- src/app/MQServices/page.jsx
+++ src/app/MQServices/page.jsx
@@ -4,6 +4,25 @@ import Body from '../components/MQServices/body'
 import Banner from '../components/MQServices/banner'
 import Contact from '../components/common/contact'
 
+export const metadata = {
+    title: "RabbitMQ Services | Comprehensive Messaging & Queuing Support",
+    description:
+      "Explore AceMQ's RabbitMQ services, including consulting, support, and professional services. Our experts ensure reliable, scalable, and efficient RabbitMQ systems.",
+    alternates: {
+      canonical: "https://acemq.com/MQServices",
+    },
+    openGraph: {
+      title: "RabbitMQ Services | Comprehensive Messaging & Queuing Support",
+      description:
+        "Explore AceMQ's RabbitMQ services, including consulting, support, and professional services. Our experts ensure reliable, scalable, and efficient RabbitMQ systems.",
+      url: "https://acemq.com/MQServices",
+      siteName: "AceMQ Consulting",
+      // Add image URL here if available
+      locale: "en_US",
+      type: "website",
+    },
+  };
+
 const MQServices = () => {
     return (
         <MaxContainer>
--- src/app/RabbitMQ/page.jsx
+++ src/app/RabbitMQ/page.jsx
@@ -8,7 +8,24 @@ import Impact from '../components/common/impact'
 import Capabilities from '../components/rabbitMQ/capabilities'
 import Contact from '../components/common/contact'
 
-
+export const metadata = {
+    title: "RabbitMQ Consulting, Support & Professional Services",
+    description:
+      "AceMQ specializes in RabbitMQ consulting, support, and professional services. Enhance your RabbitMQ systems with expert guidance and reliable solutions tailored to your needs.",
+    alternates: {
+      canonical: "https://acemq.com/rabbitmq/",
+    },
+    openGraph: {
+      title: "RabbitMQ Consulting, Support & Professional Services",
+      description:
+        "AceMQ specializes in RabbitMQ consulting, support, and professional services. Enhance your RabbitMQ systems with expert guidance and reliable solutions tailored to your needs.",
+      url: "https://acemq.com/rabbitmq/",
+      siteName: "AceMQ Consulting",
+      // Add image URL here if available
+      locale: "en_US",
+      type: "website",
+    },
+  };
 
 
 const RabbitMQ = () => {
