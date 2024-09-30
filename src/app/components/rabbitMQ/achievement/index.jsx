2773f6b104dc361a8bf1f9b00710e7a36b41be73
Abdvlljsx <alimiabdullahi93@gmail.com>
2024-09-30 19:33:32 +0100

    fixed rabbit mq desktop

--- src/app/components/rabbitMQ/achievement/index.jsx
+++ src/app/components/rabbitMQ/achievement/index.jsx
@@ -10,13 +10,13 @@ const Index = () => {
     return (
         <section className="px-[3rem] mt-[4rem] sm:mt-[10rem] sm:mb-[17rem] sm:flex sm:px-[10rem] flex-col">
             <img data-animation-id='slideIn' src="/acheivement_sm.svg" alt="info" className="sm:hidden" />
-            <div data-animation-id='slideIn' className="hidden sm:flex font-[500] items-center self-center justify-between w-[100rem]">
+            <div data-animation-id='slideIn' className="hidden sm:flex font-[500] items-center self-center justify-between sm:max-w-[120rem] gap-[8rem]">
                 <Image
                     src={'/world.png'}
                     width={350}
                     height={350}
                     alt="glob"
-                    className="w-[30rem] rounded-full border"
+                    className="w-[30rem] rounded-full"
                 />
                 <p className="w-[52rem] leading-[3rem]">
                     RabbitMQ Premier Global Services Partner. Recognized by RabbitMQ as a Go-To services partner, AceMQ maintains a global footprint, but with our headquarters in North America, we are widely recognized as the <Link href='https://www.rabbitmq.com/commercial-offerings' className="px-[.5rem] py-[.8rem] bg-[#FF6600] rounded-[.3rem]">Premier North American RabbitMQ Partner.</Link>
