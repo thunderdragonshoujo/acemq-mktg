355b6bc1b50e9a8ae5eda1d19504da13326721b9
vineetkrawat <97274573+vineetkrawat@users.noreply.github.com>
2024-10-19 11:57:26 +0530

    add thankyou page

---
+++ src/app/thank-you/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./body";
+
+export const metadata = {
+  title: "Thank you for contacting us | Ace8",
+  robots: {
+    index: false,
+    folow: true,
+  },
+};
+
+const ThankYou = () => {
+  return <Body />;
+};
+
+export default ThankYou;
